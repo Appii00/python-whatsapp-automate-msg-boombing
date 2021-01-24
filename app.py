3490f4a15c3f2a257a4b70991f24294a5c6b3cc0
Ansh rathod <67627096+Appii00@users.noreply.github.com>
2021-01-24 16:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ def main():
     driver = webdriver.Chrome(ChromeDriverManager().install())  # 2nd change
     driver.get('https://web.whatsapp.com/')
 
-    name = ""
+    name = "<who you want to send msg , >"# please enter a name what you saved in your contects and make sure you unachevied that contect
     msg = "Hello,Friend!!"
-    count = 15
+    count = 15 #here you can set msg count...................... 
 
     input('Enter any key after scanning QR code')
 
